a3e8125cddbb6880cb144d2fdab636b92d17b6a9
Stephan Plepelits <skunk@xover.mud.at>
2012-12-19 22:05:32 +0100

    Rename 'filename' -> 'name'

--- init.sql
+++ init.sql
@@ -7,13 +7,13 @@ create table directory (
 
 create table directory_link (
   directory_id  integer not null,
-  filename      text    not null,
+  name      text    not null,
   sub_directory integer null,
-  primary key(directory_id, filename)
+  primary key(directory_id, name)
 );
 
 create table file (
   directory_id  integer not null,
-  filename      text    not null,
-  primary key(directory_id, filename)
+  name      text    not null,
+  primary key(directory_id, name)
 );
