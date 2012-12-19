cd76b731cc0ee4d340ba0788a2e50e39bc872e3f
Stephan Plepelits <skunk@xover.mud.at>
2012-12-19 06:39:12 +0100

    Add conf.php-dist; configure title of application

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include "modulekit/loader.php"; /* loads all php-includes */ ?>
 <html>
   <head>
-    <title>Framework Example</title>
+    <title><?=$title?></title>
     <?php print modulekit_to_javascript(); /* pass modulekit configuration to JavaScript */ ?>
     <?php print modulekit_include_js(); /* prints all js-includes */ ?>
     <?php print modulekit_include_css(); /* prints all css-includes */ ?>
