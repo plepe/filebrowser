cc114d6788b07e98d3124b51c74fe2e7ab7b1a35
Stephan Plepelits <skunk@xover.mud.at>
2012-12-19 15:40:56 +0100

    Create sqlite database in cache-directory, initialize

---
+++ init.sql
@@ -0,0 +1,12 @@
+create table directory (
+  directory_id  integer not null,
+  archive       text    not null,
+  path          text    not null,
+  primary key(directory_id)
+);
+
+create table file (
+  directory_id  integer not null,
+  filename      text    not null,
+  primary key(directory_id, filename)
+);
