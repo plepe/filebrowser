9a64ba241536a6103a8842c5cfee2ae5f38dadac
Stephan Plepelits <skunk@xover.mud.at>
2012-12-19 06:35:31 +0100

    Add submodule 'modulekit', see https://github.com/plepe/modulekit

---
+++ modulekit.php
@@ -0,0 +1,20 @@
+<?php
+$name="Filebrowser";
+
+// an ID to identify this module
+$id="filebrowser";
+
+// these modules should be loaded first
+$depend=array();
+
+// these files will be included in this order:
+$include=array();
+$include['php']=array(
+  "inc/*.php",
+);
+$include['js']=array(
+  "inc/*.js",
+);
+$include['css']=array(
+  "inc/*.css",
+);
