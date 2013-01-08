81196f7da02ed593caf50dd333c53d3321538be7
Stephan Plepelits <skunk@xover.mud.at>
2013-01-08 08:05:00 +0100

    Search: return list of matching files

--- inc/search.php
+++ inc/search.php
@@ -15,7 +15,26 @@ class _search extends _item {
   }
 
   function content() {
-    return array();
+    global $db;
+    if(isset($this->content))
+      return $this->content;
+
+    $sql_search=$db->escapeString($this->search_text);
+
+    $res=$db->query("select directory_content.*, case when sub_directory is null then 'file' else 'directory' end as type from search_index left join directory_content on search_index.directory_id=directory_content.directory_id and search_index.name=directory_content.name where search_index.name match '{$sql_search}'");
+    while($data=$res->fetchArray()) {
+      $directory=get_directory($data['directory_id']);
+      switch($data['type']) {
+        case 'directory':
+          $this->content[]=new _directory($data['name'], $directory);
+          break;
+        case 'file':
+          $this->content[]=new _file($data['name'], $directory);
+          break;
+      }
+    }
+
+    return $this->content;
   }
 
   function url() {
