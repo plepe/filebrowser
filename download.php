c889d8072e0b636f0c7a482e40f7095503990ed4
Stephan Plepelits <skunk@xover.mud.at>
2013-01-08 21:04:27 +0100

    Download/View: set content-disposition according to script name

--- download.php
+++ download.php
@@ -8,6 +8,13 @@ $item=get_item((isset($_REQUEST['p'])?$_REQUEST['p']:null));
 $stat=$item->file_stat();
 Header("Content-Type: {$stat['mime_type']}");
 
+if(preg_match("/download\.php$/", $_SERVER['SCRIPT_NAME']))
+  $disposition_mode="attachment";
+else
+  $disposition_mode="inline";
+
+Header("Content-Disposition: {$disposition_mode}; filename=\"{$item->name()}\"");
+
 $f=$item->fopen("r");
 while($r=fread($f, 1024*1024))
   print $r;
