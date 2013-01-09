4bd39c06e8e72a6e00da12d246d51ca4a1915721
Stephan Plepelits <skunk@xover.mud.at>
2013-01-09 15:35:51 +0100

    Image Presenter: Create (and cache) thumbnail image

--- inc/image_presenter.php
+++ inc/image_presenter.php
@@ -5,6 +5,33 @@ class image_presenter extends _presenter {
       "<img src='".$this->file->download_url()."'/>".
       "</a>\n";
   }
+
+  function render_thumbnail($options=array()) {
+    return "<img src='".$this->file->thumbnail_url()."'/>";
+  }
+
+  function get_thumbnail() {
+    global $cache;
+
+    $file_name="{$cache}/{$this->file->directory_id}-{$this->file->name}";
+
+    if(file_exists($file_name))
+      return file_get_contents($file_name);
+
+    $f=$this->file->fopen("r");
+    $p=popen("convert -resize 128x128 - 'JPG:{$file_name}'", "w");
+    while($r=fgets($f))
+      fwrite($p, $r);
+
+    fclose($f);
+    pclose($p);
+
+    return file_get_contents($file_name);
+  }
+
+  function stat_thumbnail() {
+    return array("mime_type"=>"image/jpeg");
+  }
 }
 
 register_presenter("image_presenter", "~^image/(png|jpeg|gif)$~");
